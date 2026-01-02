a7c092d0fbd446faef6dd63714ce5c57eee95f73
terfaval <mate.fater@gmail.com>
2026-01-02 15:08:01 +0100

    integrated AI direction recommendations

--- app/session/[id]/work/page.tsx
+++ app/session/[id]/work/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCallback, useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
