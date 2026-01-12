1e64a191a208d1fc962932116b7a7f77dbd08727
terfaval <mate.fater@gmail.com>
2026-01-12 19:17:28 +0100

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 import { redirect } from "next/navigation";
-import { LandingPage } from "@/components/landing/LandingPage";
 import { supabaseServer } from "@/src/lib/supabase/server";
 
 const APP_ENTRY_ROUTE = "/new";
+const UNAUTH_ROUTE = "/about";
 
 export default async function Home() {
   const supabase = await supabaseServer();
   const { data } = await supabase.auth.getUser();
 
   if (data.user) redirect(APP_ENTRY_ROUTE);
-  return <LandingPage />;
+  redirect(UNAUTH_ROUTE);
 }
