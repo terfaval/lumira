70e63293c1a25dcedcc433d21ca1d876f7835e78
terfaval <mate.fater@gmail.com>
2026-01-12 22:46:30 +0100

    fixing home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-// /app/page.tsx
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import { redirect } from "next/navigation";
-import { supabaseServer } from "@/src/lib/supabase/server";
 
-const APP_ENTRY_ROUTE = "/new";
-const UNAUTH_ROUTE = "/about";
-
-export default async function Home() {
-  const supabase = await supabaseServer();
-  const { data } = await supabase.auth.getUser();
-
-  if (data.user) redirect(APP_ENTRY_ROUTE);
-  redirect(UNAUTH_ROUTE);
+export default function Home() {
+  redirect("/new");
 }
